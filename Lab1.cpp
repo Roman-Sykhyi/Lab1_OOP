08220e3ec9e085fa11aff48cf5f168a01e4d6cac
Roman-Sykhyi <kariys2015@gmail.com>
2021-02-22 10:09:36 +0200

    init

---
+++ Lab1.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+class CharSet 
+{
+private:
+	string str;
+	int strLength;
+
+public:
+	CharSet()
+	{
+		str = "";
+		strLength = 0;
+	}
+
+	CharSet(string str) 
+	{
+		SetString(str);
+	}
+	~CharSet() {}
+
+	void SetString(string str)
+	{
+		this->str = str;
+		strLength = str.length();
+	}
+
+	void Print()
+	{
+		cout << str << endl;
+	}
+
+	bool IsEqualTo(string strToCompare)
+	{
+		return str == strToCompare;
+	}
+
+	bool IsGreaterThan(string strToCompare)
+	{
+		return str > strToCompare;
+	}
+};
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	CharSet charSet("Привіт світ!");
+	//charSet.Set("Привіт світ!");
+
+	charSet.Print();
+
+	cout << charSet.IsEqualTo("Привіт світ!") << endl;
+	cout << charSet.IsGreaterThan("Привіт") << endl;
+
+	system("pause");	
+}
